26ac2923356db6cb7c8cea903eb3750cb4e896fc
Xiaoqi Yu <xiaoqiyu@Xiaoqis-MacBook-Pro.local>
2014-09-01 12:09:18 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Xiaoqi Yu on 1/9/14.
+//  Copyright (c) 2014 Xiaoqi Yu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
